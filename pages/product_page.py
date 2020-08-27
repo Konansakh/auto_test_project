116f664c604357a3ff0b38caaef188cfc33c8697
Konansakh <53832316+Konansakh@users.noreply.github.com>
2020-08-28 00:53:03 +0300

    parametrize url in test and mark it xfail

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,9 +10,9 @@ class ProductPage(BasePage):
     def should_be_successfully_added(self, name):
         assert self.is_element_present(*ProductPageLocators.ADDED_MESSAGE), "Item not added in cart"
         message = self.browser.find_element(*ProductPageLocators.ADDED_MESSAGE).text
-        assert name in message, "Added not selected book"
+        assert name == message, "Added not selected book"
 
     def should_be_basket_total_equal_price(self, price):
         assert self.is_element_present(*ProductPageLocators.BASKET_TOTAL_MESSAGE), "Basket total message not appear"
         basket_total_price = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_PRICE).text
-        assert price in basket_total_price, f"Basket total price not correct for this one book, expected: {price}, get: {basket_total_price}"
+        assert price == basket_total_price, f"Basket total price not correct for this one book, expected: {price}, get: {basket_total_price}"
